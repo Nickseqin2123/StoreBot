587a382817f3a2780a4578683e2fe1c6b1132ebb
Nikita <143941740+Nickseqin2123@users.noreply.github.com>
2024-04-29 20:38:13 +0300

    Update user_card.py

--- user/user_card.py
+++ user/user_card.py
@@ -1,16 +1,24 @@
 from aiogram import Router, F
 from SqlRequest.RequestsSecond import database
 from aiogram.types import Message
+from keyboards.keyboards import user_card_keb, menu
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 
 router = Router(name=__name__)
 
 
+class Deli(StatesGroup):
+    de = State()
+
+
 @router.message(F.text == "Корзина")
-async def card_user(message: Message):
+async def card_user(message: Message, state: FSMContext):
     card_user = database.get_user_card(message.from_user.id)
     
     if card_user:
+        await state.set_state(Deli.de)
         shet = 0
         for i in card_user:
             shet += int(i["count"]) * int(i["price"])
@@ -22,10 +30,25 @@ async def card_user(message: Message):
             )
             
         await message.answer(
-            text=f"Полна стоимость корзины = {shet} рублей"
+            text=f"Полна стоимость корзины = {shet} рублей",
+            reply_markup=user_card_keb()
         )
         
     else:
         await message.answer(
             text="Ваша корзина пуста"
         )
+
+
+@router.message(F.text == "Главное меню")
+async def go(message: Message, state: FSMContext):
+    current_state = await state.get_state()
+
+    if current_state is None:
+        return
+
+    await state.clear()
+    await message.answer(
+        text="Мы в меню",
+        reply_markup=menu(message.from_user.id)
+    )
