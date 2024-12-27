0e973a36916d6280740b4ced807cf492fa55d17c
Nikita <bletigri@gmail.com>
2024-12-27 23:29:11 +0300

    ADd first migration

--- mainstore/maindb.py
+++ mainstore/maindb.py
@@ -1,15 +1,24 @@
+from typing import Annotated
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, String
+
+
+str_256 = Annotated[str, 256]
+str64 = Annotated[str, 64]
 
 
 class Model(DeclarativeBase):
-    ...
+    type_annotation_map = {
+        str_256: String(256),
+        str64: String(64)
+    }
+    
 
 
 class Configurator:
     __USERNAME: str = 'root'
-    __PASSWORD: str = 'ZXCPUDGE228NEKITvip123'
+    __PASSWORD: str = 'ZXCPUDGE228'
     __HOST: str = '127.0.0.1'
     __DB_NAME: str = 'store'
     __PORT: int = 3306
@@ -30,7 +39,3 @@ class Configurator:
     @property
     def sync(self):
         return f'mysql+pymysql://{self.__USERNAME}:{self.__PASSWORD}@{self.__HOST}/{self.__DB_NAME}'
-
-
-con = Configurator()
-print(con.sync)
--- models/models.py
+++ models/models.py
@@ -1,20 +1,15 @@
-from typing import Annotated
-from enum import Enum
+from typing import Optional
 
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
-from mainstore.maindb import Model
-
-
-str256 = Annotated[str, 256]
-str64 = Annotated[str, 64]
+from mainstore.maindb import Model, str64, str_256
 
 
 class User(Model):
     __tablename__ = 'user'
-
-    id: Mapped[str256] = mapped_column(primary_key=True) 
+    
+    id: Mapped[str_256] = mapped_column(primary_key=True) 
 
     card = relationship('Card', back_populates='user') # ПРИМЕР: Если ты делаешь relationship для класса А из класса Б, то в классе Б дожен быть relationship для класса А
 
@@ -23,7 +18,7 @@ class Products(Model):
     __tablename__ = 'products'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str256]
+    name: Mapped[str_256]
     description: Mapped[str64]
     price: Mapped[float]
     count: Mapped[int]
@@ -35,7 +30,7 @@ class Card(Model):
     __tablename__ = 'card'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[str256] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'))
+    user_id: Mapped[str_256] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'))
     product_id: Mapped[int] = mapped_column(ForeignKey('products.id', ondelete='CASCADE'))
 
     products = relationship('Products', back_populates='card')
